f81656997cd14699723771f120eb56e04478d652
jon-shogren <jon.shogren@mailchimp.com>
2022-08-02 10:53:03 -0400

    now logs discriptions, and saves them to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,22 @@
-// set our initial variables
+// set some initial variables
 var dateHeader = $("#currentDay");
 var container = $(".container");
 var currentDay = moment();
-var currentHour = moment().format("H");
+var currentHour = moment().format("HH");
 
 // show the current day/time at the top of the page
 dateHeader.html(currentDay.format("[Today is] dddd, MMMM Do YYYY"));
 console.log("Current Hour:", currentHour);
 
-var row = "";
-
 // loop the number of hours we want to display for the work day
 // creates the rows and columns, adds styling
-for (i = 0; i <= 23; i++) {
+for (i = 8; i <= 18; i++) {
 
     row = $(`<section class="row time-block">`);
     column1 = $(`<section class="col hour">${moment().set('hour', i).format("ha")}</section>`);
-    column2 = $(`<textarea id="description" class="col description"></textarea>`);
-    column3 = $(`<section class="col saveBtn"><i id="description saveBtn" class="fas fa-save saveIcon"></i></section></section>`);
+    column2 = $(`<textarea id="description"  class="col description"></textarea>`);
+    column3 = $(`<section class="col saveBtn"><i id="saveBtn" class="fas fa-save saveIcon"></i></section></section>`);
 
-    // column2.val('');
     row.append(column1);
     row.append(column2);
     row.append(column3);
@@ -28,32 +25,37 @@ for (i = 0; i <= 23; i++) {
     // set the current hour to red
     // set all past hours to gray
     // set all future hours as green
-    if (moment().set('hour', i).format("H") === currentHour) {
+    if (moment().set('hour', i).format("HH") === currentHour) {
         column2.addClass("present");
-    } else if (moment().set('hour', i).format("H") < currentHour) {
+    } else if (moment().set('hour', i).format("HH") < currentHour) {
         column2.addClass("past");
-    } else if (moment().set('hour', i).format("H") > currentHour) {
+    } else if (moment().set('hour', i).format("HH") > currentHour) {
         column2.addClass("future");
     };
 };
 
+// set the save buttons to grab the hour and description, then save it to local storage
+$(".saveBtn").on("click", function () {
+    var hour = $(this).siblings(".hour").text();
+    var description = $(this).siblings(".description").val();
 
-var description = document.getElementById('description').value;
-console.log(description);
+    localStorage.setItem(hour, description);
+});
 
-function save() {
-    if(description === '') {
-        console.log("there's no description");
-    } else {
-        localStorage.setItem("testKey", "hello");
-    }
+function getDescriptions() {
+
+    $(".hour").each(function() {
+        var hour = $(this).text();
+        var description = localStorage.getItem(hour);
+
+        // console.log(this);
+        // console.log(currHour);
+
+        if(description !== null) {
+            $(this).siblings(".description").val(description);
+            console.log(description);
+        }
+    });
 }
 
-// save button
-var save = $(".saveIcon");
-save.click(function(){
-    save();
-    console.log("click");
-  });
-
-
+getDescriptions();
