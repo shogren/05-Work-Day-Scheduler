ea862898c8db9c5ed14c8e7af95d58d2b661dc29
jon-shogren <jon.shogren@mailchimp.com>
2022-08-02 11:54:35 -0400

    finished README, changed some comments and nesting structure

---
+++ README.md
@@ -0,0 +1,27 @@
+# 05-Work-Day-Scheduler
+
+## Description
+
+This is the module 05 "Work Day Scheduler" by Jonathan Shogren. The challange was to make an hourly calendar that could take input and store it locally on pressing save.
+
+Here is a link to the live finished page: https://shogren.github.io/05-Work-Day-Scheduler/
+
+## User Story
+
+> AS AN employee with a busy schedule
+>
+>I WANT to add important events to a daily planner
+>
+>SO THAT I can manage my time effectively
+
+## Screenshot
+
+Here is a screenshot of the finished site:
+
+![The finished webpage which has been refactored to be more accessable](./assets/images/completed.png)
+
+## Additional Notes
+
+* This was a challenging project. I started by overthinking and overengineering, and ended up starting from scratch a few times to land on the current simpler solution.
+
+* I might go back and add some functionality at some point, it'd be interesting to see this paired with future database work
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,56 @@
-// set some initial variables
-var dateHeader = $("#currentDay");
-var container = $(".container");
-var currentDay = moment();
+// set current hour
 var currentHour = moment().format("HH");
 
 // show the current day/time at the top of the page
-dateHeader.html(currentDay.format("[Today is] dddd, MMMM Do YYYY"));
+$("#currentDay").html(moment().format("[Today is] dddd, MMMM Do YYYY"));
 console.log("Current Hour:", currentHour);
 
 // loop the number of hours we want to display for the work day
 // creates the rows and columns, adds styling
-for (i = 8; i <= 18; i++) {
+function createDay() {
+    for (i = 8; i <= 18; i++) {
+        row = $(`<section class="row time-block">`);
+        column1 = $(`<section class="col hour">${moment().set('hour', i).format("ha")}</section>`);
+        column2 = $(`<textarea id="description"  class="col description"></textarea>`);
+        column3 = $(`<section class="col saveBtn"><i id="saveBtn" class="fas fa-save saveIcon"></i></section></section>`);
 
-    row = $(`<section class="row time-block">`);
-    column1 = $(`<section class="col hour">${moment().set('hour', i).format("ha")}</section>`);
-    column2 = $(`<textarea id="description"  class="col description"></textarea>`);
-    column3 = $(`<section class="col saveBtn"><i id="saveBtn" class="fas fa-save saveIcon"></i></section></section>`);
+        row.append(column1);
+        row.append(column2);
+        row.append(column3);
+        $(".container").append(row);
 
-    row.append(column1);
-    row.append(column2);
-    row.append(column3);
-    container.append(row);
-
-    // set the current hour to red
-    // set all past hours to gray
-    // set all future hours as green
-    if (moment().set('hour', i).format("HH") === currentHour) {
-        column2.addClass("present");
-    } else if (moment().set('hour', i).format("HH") < currentHour) {
-        column2.addClass("past");
-    } else if (moment().set('hour', i).format("HH") > currentHour) {
-        column2.addClass("future");
+        // set the current hour to red
+        // set all past hours to gray
+        // set all future hours as green
+        if (moment().set('hour', i).format("HH") === currentHour) {
+            column2.addClass("present");
+        } else if (moment().set('hour', i).format("HH") < currentHour) {
+            column2.addClass("past");
+        } else if (moment().set('hour', i).format("HH") > currentHour) {
+            column2.addClass("future");
+        };
     };
-};
+}
+
+createDay();
 
 // set the save buttons to grab the hour and description, then save it to local storage
 $(".saveBtn").on("click", function () {
     var hour = $(this).siblings(".hour").text();
     var description = $(this).siblings(".description").val();
-
+    
     localStorage.setItem(hour, description);
 });
 
+// loops through all of the hours, grabs descriptions from local storage and sets them if they exist
 function getDescriptions() {
-
     $(".hour").each(function() {
         var hour = $(this).text();
         var description = localStorage.getItem(hour);
 
-        // console.log(this);
-        // console.log(currHour);
-
         if(description !== null) {
             $(this).siblings(".description").val(description);
-            console.log(description);
+            console.log(description, "exists in local storage");
         }
     });
 }
